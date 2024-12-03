86932f05b5fbbea449c06e49f28c2a7d21640f03
Tavyza <theros11@proton.me>
2024-12-03 13:52:13 -0500

    doing thing

--- karledhen/mideja/gel.js
+++ karledhen/mideja/gel.js
@@ -1,29 +1,24 @@
 // il gelju midea þušy au sa ni naran
-const kadenon = ["seðysa", "ysto", "šomi", "þaša", "koža"]
-const kanan = ["ukeð", "tersi", "naki", "sosaš", "zeža", "kasað", "mešto", "yntaija", "ðesoðo", "sasað", "þareið", "retað"]
+const kadenon = ["seðysa", "ysto", "šomi", "þaša", "koža"];
+const kanan = ["ukeð", "tersi", "naki", "sosaš", "zeža", "kasað", "mešto", "yntaija", "ðesoðo", "sasað", "þareið", "retað"];
 function jazh_lakaz() {
-    //const d = new Date;
     var time = ((Date.now() / 1000)-14000)+10438416000;
-    const zh = Math.floor(time / 1.728) % 50;
-    const he = Math.floor(time / 86.4) % 100;
-    const ke = Math.floor(time / 8640) % 10;  
-    document.getElementById("sana").textContent=ke.toFixed(0) + ":" + he.toFixed(0) + ":" + zh.toFixed(0);
+    const vaus = Math.floor(time / 0.864) % 100;
+    const jazh = Math.floor(time / 86.4) % 1000; 
+    document.getElementById("jaž").textContent=jazh.toFixed(0) + ":" + vaus.toFixed(0);
     var num;
     //date
     const hykos = time / 31556926;
-    //if (hykos % 4 == 0 && hykos % 400 == 0 && hykos % 100 != 0) {
-    //    const num = 366;
-    //    const mon = 4;
-    //} else {
-    //    const num = 365;
-    //    const mon = 5;
-    //};
+    if (hykos % 4 == 0 && hykos % 400 == 0 && hykos % 100 != 0) {
+        const num = 366;
+        const mon = 4;
+    } else {
+        const num = 365;
+        const mon = 5;
+    };
     const kana = time / 86400 % 73;
     const kadeno = time / 6307200 % 5;
-    document.getElementById("reky").textContent=Math.floor(kana)+"/"+Math.floor(kadeno)+"/"+Math.floor(hykos)
+    document.getElementById("kana").textContent=Math.floor(kana)+"/"+Math.floor(kadeno)+"/"+Math.floor(hykos)
 }
 
-//while (true) {
-//    jazh_lakaz()
-//}
-setInterval(jazh_lakaz,1);
+setInterval(jazh_lakaz,64);
